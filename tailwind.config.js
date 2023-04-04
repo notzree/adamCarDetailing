2e0c900d8a1662e2023cc2ea1f191a1384739350
Annie Cai <caishajia@gmail.com>
2023-04-03 22:10:18 -0400

    ui fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
+  content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#141414",
+          "secondary": "#3d3d3d",
+          "accent": "#c1c1c1",
+          "neutral": "#fff",
+          "base-100": "#2A303C",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
