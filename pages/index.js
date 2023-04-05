4bd6768a8d8b61ec49c84201b3be54fdc9553841
Annie Cai <caishajia@gmail.com>
2023-04-05 13:17:45 -0400

    fixed image load + corner

--- pages/index.js
+++ pages/index.js
@@ -203,6 +203,7 @@ export default function Home({ }) {
                       src={`${item.img}?w=248&fit=crop&auto=format`}
                       srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
                       alt={item.title}
+                      className="rounded-lg"
                     />
                   </ImageListItem>
                 ))}
